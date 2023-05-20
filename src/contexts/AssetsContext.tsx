f80ed52d74663948178252ab3ced05feb02f2470
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-05-20 13:34:40 -0300

    fix: styled component and nextjs 13 ssr problem

--- src/contexts/AssetsContext.tsx
+++ src/contexts/AssetsContext.tsx
@@ -1,18 +1,5 @@
-import { IAssets } from '@/interfaces';
-import { api } from '@/pages/api';
-import { createContext, useEffect, useState } from 'react';
+import { createContext } from 'react';
 
-const [assets, setAssets] = useState<IAssets[]>([]);
 
-useEffect(() => {
-  const getAssets = async () => {
-    const { data } = await api.get('/assets')
-    setAssets(data)
-  }
 
-  getAssets();
-}, [])
-
-export const AssetsContext = createContext({
-  assets
-});
+export const AssetsContext = createContext(1);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import { AssetsContext } from "@/contexts/AssetsContext"
 import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 import { useContext, useEffect } from "react"
 import { api } from "./api"
 
 export default function Home() {
-  const { assets } = useContext(AssetsContext);
-
-  console.log(assets)
-
   useEffect(() => {
     const getAssets = async () => {
       const { data } = await api.get('/assets')
