61e6846cd44604b879f15d0f2bca58324741b0e2
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-05-19 22:20:41 -0300

    feat: creating theme and testing api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
+import { useEffect } from "react"
+import { api } from "./api"
 
 const options = {
   title: {
@@ -11,13 +13,22 @@ const options = {
 }
 
 export default function Home() {
+  useEffect(() => {
+    const getAssets = async () => {
+      const { data } = await api.get('/assets')
+      console.log(data)
+    }
+
+    getAssets();
+  }, [])
+
   return (
     <div>
       <HighchartsReact
         highcharts={Highcharts}
         options={options}
       />
-    <h1>Hello Tractian!</h1>
+      <h1>Hello Tractian!</h1>
     </div>
   )
 }
