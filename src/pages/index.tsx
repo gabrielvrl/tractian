2d26bd38de72664880373fa944193a2d474e4cc3
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-05-19 21:58:12 -0300

    feat: adding and getting to know highcharts lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Highcharts from "highcharts"
+import HighchartsReact from "highcharts-react-official"
 
-const inter = Inter({ subsets: ['latin'] })
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
+}
 
 export default function Home() {
   return (
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
     <h1>Hello Tractian!</h1>
+    </div>
   )
 }
